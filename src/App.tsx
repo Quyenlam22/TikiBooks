a3181e194cc1b4b820be679dae5eba3bd77e27b8
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-08-12 16:03:18 +0700

    Pagiantion

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
-import RouterClient from './components/AllRouter/RouterClient'
-import DetailBook from './pages/DetailBook'
-import Home from './pages/Home'
 import AllRouter from './components/AllRouter'
 
 function App() {
   return (
     <>
-      <AllRouter />
+      <AllRouter/>
     </>
   )
 }
 
-export default App
+export default App;
